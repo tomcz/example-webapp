a4578c262b6895c895d66e76facbf4c823677595
tomcz <github@tomczarniecki.com>
2010-04-07 23:18:55 +1000

    force use of group name

--- src/main/example/spring/template/TemplateViewFactory.java
+++ src/main/example/spring/template/TemplateViewFactory.java
@@ -1,8 +1,5 @@
 package example.spring.template;
 
 public interface TemplateViewFactory {
-
-    TemplateView create(String templateName);
-
     TemplateView create(String groupName, String templateName);
 }
