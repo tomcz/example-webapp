fc4c1bbdb590a43beaed9443687e2634640259b6
tomcz <github@tomczarniecki.com>
2010-10-17 13:57:47 +1100

    updated to spring-stringtemplate 1.3 libraries

--- src/unit-tests/example/spring/view/PathRendererTests.java
+++ src/unit-tests/example/spring/view/PathRendererTests.java
@@ -3,6 +3,7 @@ package example.spring.view;
 import example.spring.Path;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -11,11 +12,12 @@ public class PathRendererTests {
 
     @Test
     public void shouldPrependContextPathAndServletPath() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setContextPath("/context");
         request.setServletPath("/servlet");
 
-        PathRenderer renderer = new PathRenderer(request);
+        PathRenderer renderer = new PathRenderer(request, response);
         String text = renderer.toString(new Path("/path", true, true), "none");
 
         assertThat(text, is("/context/servlet/path"));
@@ -23,11 +25,12 @@ public class PathRendererTests {
 
     @Test
     public void shouldPrependContextPath() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setContextPath("/context");
         request.setServletPath("/servlet");
 
-        PathRenderer renderer = new PathRenderer(request);
+        PathRenderer renderer = new PathRenderer(request, response);
         String text = renderer.toString(new Path("/path", true, false), "none");
 
         assertThat(text, is("/context/path"));
@@ -35,11 +38,12 @@ public class PathRendererTests {
 
     @Test
     public void shouldPrependServletPath() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setContextPath("/context");
         request.setServletPath("/servlet");
 
-        PathRenderer renderer = new PathRenderer(request);
+        PathRenderer renderer = new PathRenderer(request, response);
         String text = renderer.toString(new Path("/path", false, true), "none");
 
         assertThat(text, is("/servlet/path"));
